c5b101d716ad1d311a3cc9f03217d6184d3708f1
Akshay Phadke <aphadke93@gmail.com>
2018-02-24 14:17:06 -0800

    Refactor keyboard input

--- kilo.c
+++ kilo.c
@@ -46,6 +46,27 @@ void enable_raw_mode(){
     die("tcsetattr - enable_raw_mode");
 }
 
+char editor_read_key(){
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1){
+    if (nread == -1 && errno != EAGAIN){
+      die("read");
+    }
+  }
+  return c;
+}
+
+/*-----INPUT--------------------------------------------------*/
+
+void editor_process_keypress(){
+  char c = editor_read_key();
+  switch(c) {
+    case CTRL('q'):
+      exit(0);
+  }
+}
+
 /*-----INIT---------------------------------------------------*/
 
 int main() {
@@ -54,18 +75,7 @@ int main() {
   char c;
 
   while (1) {
-
-    c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && \
-        errno != EAGAIN)
-          die("read");
-
-    if (c == CTRL('q')) break;
-
-    if (iscntrl(c)) 
-      printf("%4d\r\n", c);
-    else
-      printf("%4d ('%c')\r\n", c, c);
+    editor_process_keypress();
   }    
 
   return 0;
