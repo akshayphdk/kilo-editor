111757a29088550fd62b6a8c1686de3bb9865e1b
Akshay Phadke <aphadke93@gmail.com>
2018-02-24 15:05:15 -0800

    Draw rows for the editor

--- kilo.c
+++ kilo.c
@@ -1,5 +1,6 @@
 /*-----INCLUDES-----------------------------------------------*/
 
+#include <sys/ioctl.h>
 #include <errno.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -9,11 +10,17 @@
 
 /*-----DEFINES------------------------------------------------*/
 
-#define CTRL(k) ((k) & 0x1F)
+#define CTRL_KEY(k) ((k) & 0x1F)
 
 /*-----DATA---------------------------------------------------*/
 
-struct termios og_termios;
+struct editor_config {
+  int screen_rows;
+  int screen_cols;
+  struct termios og_termios;
+};
+
+struct editor_config CONF;
 
 /*-----TERMINAL-----------------------------------------------*/
 
@@ -25,17 +32,17 @@ void die(const char* s){
 }
 
 void disable_raw_mode(){
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &og_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &CONF.og_termios) == -1)
     die("tcsetattr - disable_raw_mode");
 }
 
 void enable_raw_mode(){
-  if (tcgetattr(STDIN_FILENO, &og_termios) == -1)
+  if (tcgetattr(STDIN_FILENO, &CONF.og_termios) == -1)
     die("tcgetattr - enable_raw_mode");
 
   atexit(disable_raw_mode);
 
-  struct termios raw = og_termios;
+  struct termios raw = CONF.og_termios;
   raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
@@ -59,12 +66,23 @@ char editor_read_key(){
   return c;
 }
 
+int get_window_size(int* rows, int* cols){
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || \
+      ws.ws_col == 0){ return -1;}
+  else {
+    *rows = ws.ws_row;
+    *cols = ws.ws_col;
+    return 0;
+  }
+}
+
 /*-----INPUT--------------------------------------------------*/
 
 void editor_process_keypress(){
   char c = editor_read_key();
   switch(c) {
-    case CTRL('q'):
+    case CTRL_KEY('q'):
       write(STDOUT_FILENO, "\x1B[2J", 4);
       write(STDOUT_FILENO, "\x1B[H", 3);
       exit(0);
@@ -73,17 +91,30 @@ void editor_process_keypress(){
 
 /*-----OUTPUT-------------------------------------------------*/
 
+void editor_draw_rows(){
+  for(int y=0; y<CONF.screen_cols; y++){
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
 void editor_refresh_screen(){
   write(STDOUT_FILENO, "\x1B[2J", 4);
+  write(STDOUT_FILENO, "\x1B[H", 3);
+  editor_draw_rows();
   write(STDOUT_FILENO, "\x1B[H", 3);
 }
 
 /*-----INIT---------------------------------------------------*/
 
+void init_editor(){
+  if (get_window_size(&CONF.screen_rows,&CONF.screen_cols) == -1)
+    die("get_window_size");
+}
+
 int main() {
 
   enable_raw_mode();
-  char c;
+  init_editor();
 
   while (1) {
     editor_refresh_screen();
