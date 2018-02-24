d60292af8e1b76a1e14897f5f456769ee0245baa
Akshay Phadke <aphadke93@gmail.com>
2018-02-24 13:03:01 -0800

    Raw mode - add timeout for read()

--- kilo.c
+++ kilo.c
@@ -19,18 +19,30 @@ void enable_raw_mode(){
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 
 int main() {
+
   enable_raw_mode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+
+  while (1) {
+    c = '\0';
+    read(STDIN_FILENO, &c, 1);
+
     if (iscntrl(c)) 
       printf("%4d\r\n", c);
     else
       printf("%4d ('%c')\r\n", c, c);
+
+    if (c == 'q') break;
   }    
+
   return 0;
 }
