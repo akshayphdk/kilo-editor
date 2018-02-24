cb95c71715eb57106ba122612b1dc4eeeb083a75
Akshay Phadke <aphadke93@gmail.com>
2018-02-24 11:15:10 -0800

    Raw mode - display key presses

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -21,6 +23,11 @@ void enable_raw_mode(){
 int main() {
   enable_raw_mode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    if (iscntrl(c)) 
+      printf("%4d\n", c);
+    else
+      printf("%4d ('%c')\n", c, c);
+  }    
   return 0;
 }
