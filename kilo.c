8ddbec101e212e326738794d0fe6629952b0b42b
Akshay Phadke <aphadke93@gmail.com>
2018-02-24 18:37:30 -0800

    Buffer implementation for screen refresh

--- kilo.c
+++ kilo.c
@@ -1,5 +1,6 @@
 /*-----INCLUDES-----------------------------------------------*/
 
+#include <string.h>
 #include <sys/ioctl.h>
 #include <errno.h>
 #include <ctype.h>
@@ -21,6 +22,27 @@ struct editor_config {
 };
 
 struct editor_config CONF;
+
+/*-----APPEND BUFFER------------------------------------------*/
+
+#define ABUF_INIT {NULL, 0}
+struct abuf {
+  char *b;
+  int len;
+};
+
+void abuf_append(struct abuf* ab, const char* s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+ 
+void abuf_free(struct abuf* ab){
+  free(ab->b);
+}
 
 /*-----TERMINAL-----------------------------------------------*/
 
@@ -66,10 +88,34 @@ char editor_read_key(){
   return c;
 }
 
+int get_cursor_position(int* rows, int* cols) {
+
+  char buf[32];
+  unsigned int i = 0;
+
+  if(write(STDOUT_FILENO, "\x1B[6n", 4) != 4) return -1;
+
+  while(i < sizeof(buf)-1) {
+    if(read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if(buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1B' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
 int get_window_size(int* rows, int* cols){
   struct winsize ws;
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || \
-      ws.ws_col == 0){ return -1;}
+      ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1B[999C\x1B[999B", 12) != 12)
+      { return -1;}
+    return get_cursor_position(rows, cols);
+  }
   else {
     *rows = ws.ws_row;
     *cols = ws.ws_col;
@@ -91,17 +137,26 @@ void editor_process_keypress(){
 
 /*-----OUTPUT-------------------------------------------------*/
 
-void editor_draw_rows(){
-  for(int y=0; y<CONF.screen_cols; y++){
-    write(STDOUT_FILENO, "~\r\n", 3);
+void editor_draw_rows(struct abuf* ab){
+  for(int y=0; y<CONF.screen_rows; y++){
+    abuf_append(ab, "~", 1);
+    if (y<CONF.screen_rows-1){
+      abuf_append(ab, "\r\n", 2);
+    }
   }
 }
 
 void editor_refresh_screen(){
-  write(STDOUT_FILENO, "\x1B[2J", 4);
-  write(STDOUT_FILENO, "\x1B[H", 3);
-  editor_draw_rows();
-  write(STDOUT_FILENO, "\x1B[H", 3);
+  struct abuf ab = ABUF_INIT;
+  
+  abuf_append(&ab, "\x1B[2J", 4);
+  abuf_append(&ab, "\x1B[H", 3);
+
+  editor_draw_rows(&ab);
+  abuf_append(&ab, "\x1B[H", 3);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abuf_free(&ab);
 }
 
 /*-----INIT---------------------------------------------------*/
