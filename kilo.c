a460a2ab4c8e7ee232deba22834d692192b25f8b
Akshay Phadke <aphadke93@gmail.com>
2018-08-14 18:26:01 -0700

    Opening and displaying a file

--- kilo.c
+++ kilo.c
@@ -1,3 +1,11 @@
+/*-----DEFINES------------------------------------------------*/
+
+#define _GNU_SOURCE
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define KILO_VERSION "0.0.1"
+#define CTRL_KEY(k) ((k) & 0x1F)
+
 /*-----INCLUDES-----------------------------------------------*/
 
 #include <string.h>
@@ -8,11 +16,6 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-
-/*-----DEFINES------------------------------------------------*/
-
-#define KILO_VERSION "0.0.1"
-#define CTRL_KEY(k) ((k) & 0x1F)
 
 /*-----DATA---------------------------------------------------*/
 
@@ -28,11 +31,18 @@ enum editor_keys {
   PAGE_DOWN
 };
 
+typedef struct erow {
+  int size;
+  char *chars;
+} erow;
+
 struct editor_config {
   int cx;
   int cy;
   int screen_rows;
   int screen_cols;
+  int num_rows;
+  erow *row;
   struct termios og_termios;
 };
 
@@ -181,6 +191,41 @@ int get_window_size(int* rows, int* cols){
   }
 }
 
+/*-----ROW OPERATIONS-----------------------------------------*/
+
+void editor_append_row(char *s, size_t len) {
+
+  CONF.row = realloc(CONF.row, sizeof(erow)*(CONF.num_rows+1));
+
+  int at = CONF.num_rows;
+  CONF.row[at].size = len;
+  CONF.row[at].chars = malloc(len+1);
+  memcpy(CONF.row[at].chars, s, len);
+  CONF.row[at].chars[len] = '\0';
+  CONF.num_rows++;
+}
+
+/*-----FILE I/O-----------------------------------------------*/
+
+void editor_open(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) 
+    die("fopen");
+
+  char *line = NULL;
+  size_t line_cap = 0;
+  ssize_t line_len;
+
+  while((line_len = getline(&line, &line_cap, fp)) != -1) {
+    while (line_len > 0 && 
+           (line[line_len-1] == '\n' || line[line_len-1] == '\r'))
+      line_len--;
+    editor_append_row(line, line_len);
+  }
+  free(line);
+  fclose(fp);
+}
+
 /*-----INPUT--------------------------------------------------*/
 
 void editor_move_cursor(int key){
@@ -243,26 +288,33 @@ void editor_process_keypress(){
 
 void editor_draw_rows(struct abuf* ab){
   for(int y=0; y<CONF.screen_rows; y++){
+    if (y >= CONF.num_rows) {
+      if (CONF.num_rows == 0 && y == CONF.screen_rows/2) {
 
-    if (y == CONF.screen_rows/2) {
-
-      char welcome[80];
-      int welcomelen = snprintf(welcome, sizeof(welcome),
-        "Kilo editor -- version %s", KILO_VERSION);
-      if (welcomelen > CONF.screen_cols) 
-        welcomelen = CONF.screen_cols;
-      int padding = (CONF.screen_cols - welcomelen)/2;
-      if (padding) {
-        abuf_append(ab, "~", 1);
-        padding--;
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome),
+          "Kilo editor -- version %s", KILO_VERSION);
+        if (welcomelen > CONF.screen_cols) 
+          welcomelen = CONF.screen_cols;
+        int padding = (CONF.screen_cols - welcomelen)/2;
+        if (padding) {
+          abuf_append(ab, "~", 1);
+          padding--;
+        }
+        while (padding--) 
+          abuf_append(ab, " ", 1);
+        abuf_append(ab, welcome, welcomelen);
       }
-      while (padding--) 
-        abuf_append(ab, " ", 1);
-      abuf_append(ab, welcome, welcomelen);
-    }
 
-    else
-      abuf_append(ab, "~", 1);
+      else
+        abuf_append(ab, "~", 1);
+    }
+    else {
+      int len = CONF.row[y].size;
+      if (len > CONF.screen_cols) 
+        len = CONF.screen_cols;
+      abuf_append(ab, CONF.row[y].chars, len);
+    }
 
     abuf_append(ab, "\x1B[K", 3);
     if (y<CONF.screen_rows-1){
@@ -294,15 +346,19 @@ void editor_refresh_screen(){
 void init_editor(){
   CONF.cx = 0;
   CONF.cy = 0;
+  CONF.num_rows = 0;
+  CONF.row = NULL;
   if (get_window_size(&CONF.screen_rows,&CONF.screen_cols) == -1)
     die("get_window_size");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 
   enable_raw_mode();
   init_editor();
-
+  if (argc >= 2) {
+    editor_open(argv[1]);
+  }
   while (1) {
     editor_refresh_screen();
     editor_process_keypress();
